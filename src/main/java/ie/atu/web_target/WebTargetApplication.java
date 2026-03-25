7fa3664f0725dfca91a83ec65ecef3c27abc9bec
Sami Hammar <G00393324@atu.ie>
2026-03-25 22:21:54 +0000

    Set Up

---
+++ src/main/java/ie/atu/web_target/WebTargetApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.web_target;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTargetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTargetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/web_target/WebTargetApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.web_target;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTargetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
